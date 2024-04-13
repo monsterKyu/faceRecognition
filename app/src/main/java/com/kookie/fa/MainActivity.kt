bee9834f22c8b69db34e62bc8941883926d324bb
monsterKyu <ilkyu092@naver.com>
2024-04-13 14:06:17 +0900

    first commit

---
+++ app/src/main/java/com/kookie/fa/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.kookie.fa
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.kookie.fa.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+
+            startDetectButton.setOnClickListener {
+                it.isVisible = false
+            }
+        }
+    }
+}
